ed51354f38ad65fa981f0cf1f6e140f297998153
theodor164 <tanase.ted.164@gmail.com>
2025-02-02 20:18:20 +0200

    changes...duh 1

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const daysContainer = document.querySelector('.days-container');
 const todayDay = document.querySelector('.today-day');
 const todayDate = document.querySelector('.date');
 
-const previousMonthButton = document.querySelector('.previous-month i');
-const nextMonthButton = document.querySelector('.next-month i');
+const previousMonthButton = document.querySelector('.previous-month');
+const nextMonthButton = document.querySelector('.next-month');
 
 const selectedMonthElement = document.querySelector('.selected-month');
 const selectedYearElement = document.querySelector('.selected-year');
@@ -61,7 +61,8 @@ const events = {
       'Obligativitatea de livrare a documentațiilor de urbanism și amenajarea teritoriului în noul format GIS, conform Normelor tehnice aprobate prin Ordinul nr. 904/2023.',
     time: '17:00',
     pictureLink:
-      'https://oarbucuresti.cursuri.online/Pictures/GetPictureById?pictureId=6b99a5bf-66c7-4fcf-bb00-dcd107aed837'
+      'https://oarbucuresti.cursuri.online/Pictures/GetProductPicture/aac0da9e-0852-46a4-99a0-617fc5231852',
+    link: 'https://oarbucuresti.cursuri.online/product/16e1726f-1223-4a62-847e-d25a158f6c8b/obligativitatea-de-livrare-a-documentatiilor-de-urbanism-si-amenajarea-teritoriului-in-noul-format-gis-conform-normelor-tehnice-aprobate-prin-ordinul-nr-904-2023/allbim-net'
   },
   '2025-03-25': {
     title: 'BIM | COSTS',
@@ -69,7 +70,8 @@ const events = {
       'Avantajele BIM în evaluarea costurilor unui proiect de construcții. Obligații legale și bune practici în realizarea extraselor inteligente de cantități.',
     time: '17:00',
     pictureLink:
-      'https://oarbucuresti.cursuri.online/Pictures/GetPictureById?pictureId=06935af2-5b52-4353-bb61-b10a0518a570'
+      'https://oarbucuresti.cursuri.online/Pictures/GetProductPicture/58a2511f-a126-4f30-a8ef-065730a19daf',
+    link: 'https://oarbucuresti.cursuri.online/product/9a4b53ec-b492-4c4a-a2c1-0cbecb0cd43a/avantajele-bim-in-evaluarea-costurilor-unui-proiect-de-constructii-obligatii-legale-si-bune-practici-in-realizarea-extraselor-inteligente-de-cantitati/allbim-net'
   },
   '2025-04-25': {
     title: 'BIM | DELIVERY',
@@ -77,7 +79,8 @@ const events = {
       'Cum livrezi documentația de arhitectură în format open BIM? Conformarea cu cerințele SR EN ISO 19650.',
     time: '17:00',
     pictureLink:
-      'https://oarbucuresti.cursuri.online/Pictures/GetPictureById?pictureId=06935af2-5b52-4353-bb61-b10a0518a570'
+      'https://oarbucuresti.cursuri.online/Pictures/GetProductPicture/415cc971-e340-473e-9e70-e9d1a4a7f3eb',
+    link: 'https://oarbucuresti.cursuri.online/product/85b4d7e4-d896-43b0-a80f-553ebac3d5ef/cum-livrezi-documentatia-de-arhitectura-in-format-open-bim-conformarea-cu-cerintele-sr-en-iso-19650/allbim-net'
   },
   '2025-05-27': {
     title: 'BIM | RENDER',
@@ -85,7 +88,8 @@ const events = {
       'Tandemul ALLPLAN AI Render și Lumion pentru construirea imaginilor de prezentare imersive.',
     time: '17:00',
     pictureLink:
-      'https://oarbucuresti.cursuri.online/Pictures/GetPictureById?pictureId=06935af2-5b52-4353-bb61-b10a0518a570'
+      'https://oarbucuresti.cursuri.online/Pictures/GetProductPicture/0dd4bf0b-d412-4fe0-8f84-12e50949e26b',
+    link: 'https://oarbucuresti.cursuri.online/product/cbb45e81-8a0d-4c16-b271-96cb95c96966/tandemul-allplan-ai-render-si-lumion-pentru-construirea-imaginilor-de-prezentare-imersive/allbim-net'
   },
   '2025-06-24': {
     title: 'BIM | LIGHT',
@@ -93,7 +97,8 @@ const events = {
       'Digitalizarea studiului de însorire: respectarea reglementărilor și cerințelor esențiale pentru documentația de proiect.',
     time: '17:00',
     pictureLink:
-      'https://oarbucuresti.cursuri.online/Pictures/GetPictureById?pictureId=06935af2-5b52-4353-bb61-b10a0518a570'
+      'https://oarbucuresti.cursuri.online/Pictures/GetProductPicture/bcb0110d-1590-4a2a-b1cd-6c844761ec7b',
+    link: 'https://oarbucuresti.cursuri.online/product/a25c3d64-10f1-4a49-9fc6-c2a786e78578/digitalizarea-studiului-de-insorire-si-umbrire-respectarea-reglementarilor-si-cerintelor-esentiale-pentru-documentatia-de-proiect/allbim-net'
   },
   '2025-07-29': {
     title: 'BIM | NZEB',
@@ -101,7 +106,8 @@ const events = {
       'Conformarea energetică a anvelopei NZEB în acord cu metodologia MC-001/2022.',
     time: '17:00',
     pictureLink:
-      'https://oarbucuresti.cursuri.online/Pictures/GetPictureById?pictureId=06935af2-5b52-4353-bb61-b10a0518a570'
+      'https://oarbucuresti.cursuri.online/Pictures/GetProductPicture/b83120f6-21bb-46a0-b2d4-a06ddcedd7fc',
+    link: 'https://oarbucuresti.cursuri.online/product/b2e783e1-a92d-41ed-ae12-7fddfc7fe66c/conformarea-energetica-a-anvelopei-nzeb-in-acord-cu-metodologia-mc-001-2022/allbim-net'
   },
   '2025-09-23': {
     title: 'BIM | RESTORATION',
@@ -109,7 +115,8 @@ const events = {
       'Monumente istorice: Instrumente pentru digitalizarea istoriei construite prin nori de puncte si fotogrametrie.',
     time: '17:00',
     pictureLink:
-      'https://oarbucuresti.cursuri.online/Pictures/GetPictureById?pictureId=06935af2-5b52-4353-bb61-b10a0518a570'
+      'https://oarbucuresti.cursuri.online/Pictures/GetProductPicture/093e419c-c764-40b6-93e3-09223f3b4c28',
+    link: 'https://oarbucuresti.cursuri.online/product/fc71b576-bb83-4a19-b9e8-d778591b6b18/monumente-istorice-instrumente-pentru-digitalizarea-istoriei-construite-prin-nori-de-puncte-si-fotogrametrie/allbim-net'
   },
   '2025-10-28': {
     title: 'BIM | PARAMETRIC',
@@ -117,7 +124,8 @@ const events = {
       'Impactul tehnologiei în proiectarea de arhitectură: unelte pentru programare vizuală și parametrizare.',
     time: '17:00',
     pictureLink:
-      'https://oarbucuresti.cursuri.online/Pictures/GetPictureById?pictureId=06935af2-5b52-4353-bb61-b10a0518a570'
+      'https://oarbucuresti.cursuri.online/Pictures/GetProductPicture/99fca7bf-9718-436f-8b85-dd06d2807f0b',
+    link: 'https://oarbucuresti.cursuri.online/product/ff4f2e6b-f29b-4f23-9d2d-772e48e284fe/impactul-tehnologiei-in-proiectarea-de-arhitectura-unelte-pentru-programare-vizuala-si-parametrizare/allbim-net'
   }
 };
 
@@ -163,6 +171,7 @@ function resetDaysContainer(wrapper = daysWrapper) {
 }
 
 loadCalendar(firstDay, daysInMonth, month, year);
+highlightSelectedDay(closestEvent.date);
 
 function loadCalendar(
   firstDay,
@@ -188,7 +197,9 @@ function loadCalendar(
       day,
       isTodayFlag ? 'marked-as-today' : '',
       eventFlag,
-      wrapper
+      wrapper,
+      selectedYear,
+      selectedMonth
     );
     dayIndex++;
   }
@@ -213,13 +224,18 @@ function populateDay(
   text,
   additionalClass = '',
   eventClass = '',
-  wrapper = daysWrapper
+  wrapper = daysWrapper,
+  year,
+  month
 ) {
   const dayElement = wrapper.children[index];
   dayElement.textContent = text;
   dayElement.className = `day ${additionalClass} ${eventClass}`;
 
   // const newDayElement = dayElement.cloneNode(true); // Remove event listeners of the days in the previous and next month
+
+  const dateString = new Date(year, month, text).toLocaleDateString('en-CA');
+  dayElement.setAttribute('data-date', dateString);
 
   if (dayElement.classList.contains('event-day')) {
     setEventDayClickHandler(dayElement);
@@ -239,7 +255,10 @@ function handleEventDisplay(event) {
   ).toLocaleDateString('en-CA');
 
   if (event.target.classList.contains('event-day')) {
-    showEventDetails({ date: eventDate, ...events[eventDate] });
+    const eventDetails = { date: eventDate, ...events[eventDate] };
+    showEventDetails(eventDetails);
+    // Highlight the day in the calendar
+    highlightSelectedDay(eventDate);
   }
 }
 
@@ -253,7 +272,6 @@ function showEventDetails(event) {
     ? 'Urmatorul curs va fi:'
     : 'Te poti inscrie si la:';
 
-
   eventDateElement.innerHTML = `
   <svg class="svg-inline--fa fa-calendar-days" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="calendar-days" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M128 0c17.7 0 32 14.3 32 32l0 32 128 0 0-32c0-17.7 14.3-32 32-32s32 14.3 32 32l0 32 48 0c26.5 0 48 21.5 48 48l0 48L0 160l0-48C0 85.5 21.5 64 48 64l48 0 0-32c0-17.7 14.3-32 32-32zM0 192l448 0 0 272c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 192zm64 80l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm128 0l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM64 400l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zm112 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16z"></path></svg>
   ${eventDate}
@@ -263,10 +281,7 @@ function showEventDetails(event) {
   eventHourElement.innerHTML =
     '<svg class="svg-inline--fa fa-clock-five" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="clock-five" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm24-392l0 128.7 60 90c7.4 11 4.4 25.9-6.7 33.3s-25.9 4.4-33.3-6.7l-64-96c-2.6-3.9-4-8.6-4-13.3l0-136c0-13.3 10.7-24 24-24s24 10.7 24 24z"></path></svg> Ora ' +
     event.time;
-  eventButtonElement.setAttribute(
-    'href',
-    'http://www.facebook.com/theodor.tanase'
-  );
+  eventButtonElement.setAttribute('href', `${event.link}`);
   eventPictureElement.setAttribute('src', event.pictureLink);
   eventButtonElement.innerHTML =
     '<svg class="svg-inline--fa fa-arrow-right-long" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right-long" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"></path></svg> Inscrie-te la acest curs';
@@ -278,9 +293,9 @@ previousMonthButton.addEventListener('click', handleMonthChange);
 nextMonthButton.addEventListener('click', handleMonthChange);
 
 function handleMonthChange(event) {
-  if (event.target.classList.contains('fa-chevron-left')) {
+  if (event.target.classList.contains('previous-month')) {
     animateMonthChange(false);
-  } else if (event.target.classList.contains('fa-chevron-right')) {
+  } else if (event.target.classList.contains('next-month')) {
     animateMonthChange(true);
   } else if (
     event.target.classList.contains('date') ||
@@ -351,6 +366,7 @@ function animateMonthChange(isNext) {
     nextWrapper.classList.remove(
       animationClass === 'slide-left' ? 'slide-right' : 'slide-left'
     );
+    highlightSelectedDay(closestEvent.date);
     // }, 1000);
   }, 200);
 }
@@ -424,5 +440,19 @@ function handleSwipe() {
   } else if (touchEndX - touchStartX > swipeThreshold) {
     animateMonthChange(false);
   }
+}
 
+function highlightSelectedDay(eventDateString) {
+  // Remove highlight from any previously highlighted day
+  document.querySelectorAll('.day.highlighted-event').forEach((el) => {
+    el.classList.remove('highlighted-event');
+  });
+
+  // Find the day element that has the matching data-date attribute
+  const dayElement = document.querySelector(
+    `.day[data-date="${eventDateString}"]`
+  );
+  if (dayElement) {
+    dayElement.classList.add('highlighted-event');
+  }
 }
